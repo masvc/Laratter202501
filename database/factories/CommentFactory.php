607c3ae8f84cee9b406bb7a343146660fe68305e
mas <m.yoshida553@gmail.com>
2025-01-19 20:19:35 +0900

    comment function add&test

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+// 🔽 2行追加
+use App\Models\User;
+use App\Models\Tweet;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +19,11 @@ class CommentFactory extends Factory
      */
     public function definition(): array
     {
+        // 🔽 追加
         return [
-            //
+            'comment' => fake()->sentence,
+            'user_id' => User::factory(),
+            'tweet_id' => Tweet::factory(),
         ];
     }
 }
