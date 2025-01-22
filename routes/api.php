51682f1cde4aa0243f8ca6cf2b8537e21406561a
mas <m.yoshida553@gmail.com>
2025-01-22 13:06:09 +0900

    Like機能の機能のAPI実装とテスト

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 // 🔽 追加
 use App\Http\Controllers\Api\TweetController;
+// 🔽 追加
+use App\Http\Controllers\Api\TweetLikeController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -15,7 +17,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// 🔽 追加
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tweets', TweetController::class);
+    // 🔽 2 行追加
+    Route::post('/tweets/{tweet}/like', [TweetLikeController::class, 'store']);
+    Route::delete('/tweets/{tweet}/like', [TweetLikeController::class, 'destroy']);
 });
