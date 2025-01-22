1e62c2da9b8ec30ad4af70c038569e8b05bcb990
mas <m.yoshida553@gmail.com>
2025-01-22 11:59:25 +0900

    ログインログアウトのAPIAPI実装

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+// routes/api.php
+
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+// 🔽 追加
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
