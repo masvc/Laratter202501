f814f80a2b9bc774af43a8964c084e1d41a90f24
mas <m.yoshida553@gmail.com>
2025-01-22 12:32:30 +0900

    Tweet CRUD 処理の API 実装

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,21 @@
-// routes/api.php
-
 <?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+// 🔽 追加
+use App\Http\Controllers\Api\TweetController;
 
 Route::post('/register', [AuthController::class, 'register']);
-// 🔽 追加
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
+})->middleware('auth:sanctum');
+
+// 🔽 追加
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tweets', TweetController::class);
 });
